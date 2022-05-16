d89f0d4ad7b5d9e44595d2a6194f9c7876f4fa8b
ercanon <61042236+ercanon@users.noreply.github.com>
2022-05-16 11:55:27 +0200

    organization

---
+++ Ultra_Hot/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    /*
+    public InputActionReference horizontalLook;
+    public InputActionReference verticalLook;
+    */
+    public float lookSpeed =1f;
+    public Transform cameraTransform;
+    float pitch;
+    float yaw;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
