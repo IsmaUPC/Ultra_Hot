cb478a2ef41851b5d98a3d19326a61762212b9f7
IsmaUPC <61043099+IsmaUPC@users.noreply.github.com>
2022-05-26 12:12:32 +0200

    BulletTime
    Co-Authored-By: Alejandro Moreno <61042882+JandroK@users.noreply.github.com>

--- Ultra_Hot/Assets/BulletTime.cs
+++ Ultra_Hot/Assets/BulletTime.cs
@@ -7,14 +7,14 @@ public class BulletTime : MonoBehaviour
     public float speed;
     public Rigidbody handL;
     public Rigidbody handR;
-    private Vector3 lastPosition;
+    private Vector3 vecDir = Vector3.zero;
     private Rigidbody rb;
-    public int multSpeed = 10;
+    public int multSpeed = 50;
 
     private void Start()
     {
         //lastPosition = transform.position;
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
@@ -25,14 +25,26 @@ public class BulletTime : MonoBehaviour
     private void Update()
     {
         CalculateSpeed();
-        rb.velocity = rb.velocity.normalized * speed * multSpeed * Time.deltaTime;
-        rb.angularVelocity = rb.angularVelocity.normalized * speed * multSpeed * Time.deltaTime;
-        //transform.position += (transform.position - lastPosition) * speed * Time.deltaTime;
+        //rb.velocity = rb.velocity.normalized * speed * multSpeed * Time.deltaTime;
+        //rb.angularVelocity = rb.angularVelocity.normalized * speed * multSpeed * Time.deltaTime;
+        transform.position += vecDir * speed * Time.deltaTime;
+        if(vecDir != Vector3.zero)
+        {
+            //vecDir += new Vector3(0, Physics.gravity.y, 0) * Time.deltaTime*0.5f;
+        }
         //lastPosition = transform.position;
     }
 
     void CalculateSpeed()
     {
         speed = handL.velocity.magnitude + handR.velocity.magnitude;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            vecDir = collision.relativeVelocity;
+        }
     }
 }
