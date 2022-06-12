1cda702f448fc2c045cf907010bc89b827b36516
MHF13 <41444696+MHF13@users.noreply.github.com>
2022-06-12 20:12:58 +0200

    Button Bug solved

---
+++ Ultra_Hot/Assets/StartLevel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Autohand.Demo
+{
+    public class StartLevel : MonoBehaviour
+    {
+        public void StartGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetSceneByBuildIndex(1).ToString());
+        }
+    }
+}
