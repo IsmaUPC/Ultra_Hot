7dba61b616e292ec75f6b0a6d64be9d8e5df3779
JandroK <61042882+JandroK@users.noreply.github.com>
2022-06-13 01:21:06 +0200

    Destroy dice Guns and Sword

--- Ultra_Hot/Assets/Levitation.cs
+++ Ultra_Hot/Assets/Levitation.cs
@@ -2,35 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Levitation : MonoBehaviour
+namespace Autohand.Demo
 {
-    // Start is called before the first frame update
-    public float speedLevi = 1;
-    float angle = 10;
-    Vector3 localPos;
-    float localPosY;
-    public float distance = 0.4f;
-    public bool up = true;
-
-    void Start()
+    public class Levitation : MonoBehaviour
     {
-        localPos = transform.localPosition;   
-        localPosY = transform.localPosition.y;   
-    }
+        // Start is called before the first frame update
+        public float speedLevi = 1;
+        float angle = 10;
+        Vector3 localPos;
+        float localPosY;
+        public float distance = 0.4f;
+        public bool up = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(up)
+        void Start()
         {
-            localPos.y = distance * Mathf.Sin(angle) + localPosY;
-            transform.localPosition = localPos;
-            angle += Time.deltaTime * speedLevi;
-        }        
-    }
+            localPos = transform.localPosition;
+            localPosY = transform.localPosition.y;
+        }
 
-    public void StopLevitation()
-    {
-        up = false;
+        // Update is called once per frame
+        void Update()
+        {
+            if (up)
+            {
+                localPos.y = distance * Mathf.Sin(angle) + localPosY;
+                transform.localPosition = localPos;
+                angle += Time.deltaTime * speedLevi;
+            }
+        }
+
+        public void StopLevitation()
+        {
+            up = false;
+        }
     }
 }
--- Ultra_Hot/Assets/Plugins/AutoHand/Scripts/Demo/Demo Scene/CubeBreak.cs
+++ Ultra_Hot/Assets/Plugins/AutoHand/Scripts/Demo/Demo Scene/CubeBreak.cs
@@ -3,11 +3,26 @@ using UnityEngine;
 namespace Autohand.Demo{
     public class CubeBreak : MonoBehaviour{
         [SerializeField] private GameObject enemiesLevel;
-
         public float force = 10f;
+        [Header("Rotation")]
+        private bool rot = true;
+        public float rotateSpeed = 10;
+        [Header("Levitation")]
+        public float speedLevi = 1;
+        float angle = 10;
+        Vector3 localPos;
+        float localPosY;
+        public float distance = 0.1f;
+        public bool up = true;
+
         Vector3[] offsets = { new Vector3(0.25f, 0.25f, 0.25f), new Vector3(-0.25f, 0.25f, 0.25f), new Vector3(0.25f, 0.25f, -0.25f), new Vector3(-0.25f, 0.25f, -0.25f),
                             new Vector3(0.25f, -0.25f, 0.25f), new Vector3(-0.25f, -0.25f, 0.25f), new Vector3(0.25f, -0.25f, -0.25f), new Vector3(-0.25f, -0.25f, -0.25f),};
         [ContextMenu("Break")]
+        private void Start()
+        {
+            localPos = transform.localPosition;
+            localPosY = transform.localPosition.y;
+        }
         public void Break() {
             for(int i = 0; i < 8; i++) {
                 var smallerCopy = Instantiate(gameObject, transform.position, transform.rotation);
@@ -22,6 +37,7 @@ namespace Autohand.Demo{
                 smallerCopy.transform.parent = null;
                 smallerCopy.transform.localScale = transform.localScale/2f;
                 smallerCopy.layer = LayerMask.NameToLayer(Hand.grabbableLayerNameDefault);
+                smallerCopy.GetComponent<Rigidbody>().useGravity = true;
                 Destroy(smallerCopy.GetComponent<CubeBreak>());
                 var body = smallerCopy.GetComponent<Rigidbody>();
                 body.ResetCenterOfMass();
@@ -37,6 +53,39 @@ namespace Autohand.Demo{
             enemiesLevel.SetActive(true);
 
             Destroy(gameObject);
+        }
+
+        private void Update()
+        {
+            if (rot)
+                transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed, Space.World);
+
+            if (up)
+            {
+                localPos.y = distance * Mathf.Sin(angle) + localPosY;
+                transform.localPosition = localPos;
+                angle += Time.deltaTime * speedLevi;
+            }
+        }
+        public void StopRotLev()
+        {
+            rot = false;
+            up = false;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Bullet"))
+            {
+                Break();
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("sword")){
+                Break();
+            }
         }
 
     }
