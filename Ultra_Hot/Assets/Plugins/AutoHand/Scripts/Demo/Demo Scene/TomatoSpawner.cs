429f8d10b8072725647b4b3b099001959d4fe549
MHF13 <41444696+MHF13@users.noreply.github.com>
2022-06-12 20:05:25 +0200

    Start Scene

--- Ultra_Hot/Assets/Plugins/AutoHand/Scripts/Demo/Demo Scene/TomatoSpawner.cs
+++ Ultra_Hot/Assets/Plugins/AutoHand/Scripts/Demo/Demo Scene/TomatoSpawner.cs
@@ -1,26 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-namespace Autohand.Demo{
-public class TomatoSpawner : MonoBehaviour{
-    public GameObject[] tomatoes;
-    List<GameObject> copies;
+namespace Autohand.Demo
+{
+    public class TomatoSpawner : MonoBehaviour
+    {
+        public GameObject[] tomatoes;
+        List<GameObject> copies;
 
-    void Start(){
-        copies = new List<GameObject>();
-        foreach(var tomato in tomatoes) {
-            var newTomato = Instantiate(tomato, tomato.transform.position, tomato.transform.rotation);
-            newTomato.transform.position += new Vector3(0, 0.2f, 0);
-            newTomato.SetActive(false);
-            copies.Add(newTomato);
-            
+        void Start()
+        {
+            copies = new List<GameObject>();
+            foreach (var tomato in tomatoes)
+            {
+                var newTomato = Instantiate(tomato, tomato.transform.position, tomato.transform.rotation);
+                newTomato.transform.position += new Vector3(0, 0.2f, 0);
+                newTomato.SetActive(false);
+                copies.Add(newTomato);
+
+            }
+        }
+
+        public void SpawnTomato()
+        {
+            int i = Random.Range(0, copies.Count - 1);
+            Instantiate(copies[i], copies[i].transform.position, copies[i].transform.rotation).SetActive(true);
+        }
+        public void StartGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetSceneByBuildIndex(1).ToString());
         }
     }
-
-    public void SpawnTomato() {
-        int i = Random.Range(0, copies.Count-1);
-        Instantiate(copies[i], copies[i].transform.position, copies[i].transform.rotation).SetActive(true);
-    }
-}
 }
