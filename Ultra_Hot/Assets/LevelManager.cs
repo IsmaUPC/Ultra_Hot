ba678af286284c8366dde7e61ea2f0158d13699c
JandroK <61042882+JandroK@users.noreply.github.com>
2022-06-12 18:32:44 +0200

    LevelManager
    Co-Authored-By: IsmaUPC <61043099+IsmaUPC@users.noreply.github.com>

---
+++ Ultra_Hot/Assets/LevelManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Autohand
+{
+    public class LevelManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public TeleportManager tp;
+        public List<GameObject> levels;
+        public List<GameObject> dices;
+        public int currentLevel = 0;
+
+        private EnemyScript[] enemies;
+        private int count = 0;
+
+        void Start()
+        {
+            enemies = levels[currentLevel].GetComponentsInChildren<EnemyScript>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            count = 0;
+            foreach (var item in enemies)
+            {
+                if (item.dead)
+                    count++;
+            }
+            if (count == enemies.Length && currentLevel < levels.Count)
+            {
+                currentLevel++;
+                enemies = levels[currentLevel].GetComponentsInChildren<EnemyScript>();
+                dices[currentLevel].SetActive(true);
+                tp.ActiveTelepor();
+            }
+        }
+    }
+}
+    
