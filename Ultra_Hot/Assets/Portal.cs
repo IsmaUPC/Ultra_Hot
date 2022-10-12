f06c19847439828a76f50e29bc399bba5ad38099
David González <DavidGonzalez2001-9@hotmail.com>
2022-10-12 18:24:36 +0200

    Enemy Portals

---
+++ Ultra_Hot/Assets/Portal.cs
@@ -0,0 +1,51 @@
+using DG.Tweening;
+using DG.Tweening.Core.Easing;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    bool scaling = false;
+    bool endedScaling = false;
+    float x, y, z;
+
+    [SerializeField] float xSpeed = 0.5f;
+    [SerializeField] float ySpeed = 0.5f;
+    [SerializeField] float lifetime = 1.0f;
+
+    float count = 0;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(0.35f, 0.02f, 0.05f);
+        x = transform.localScale.x;
+        y = transform.localScale.y;
+        z = transform.localScale.z;
+
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Static"))
+        {
+            if (count <= lifetime)
+            {
+                count += Time.deltaTime;
+                Debug.Log(count);
+            }
+            else
+            {
+                //anim.SetBool("exiting", true);
+                anim.Play("Exiting");
+            }
+        }
+    }
+
+
+}
